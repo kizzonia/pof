9c64a82d06e4c286f2ed678b12607cd92ccff2d9
fortune chibuike <chibuikefortune@hotmail.com>
2017-10-07 16:57:54 +0100

    done work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,7 @@ class WelcomeController < ApplicationController
       @welcome = Welcome.new(params[:welcome])
       @welcome.request = request
       if @welcome.deliver
-        redirect_to "https://pof.com", flash.now[:error] = nil
+        redirect_to "https://pof.com"
       else
         flash.now[:error] = 'cannot send message.'
         render :index
