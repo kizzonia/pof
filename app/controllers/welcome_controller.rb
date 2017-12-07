2626f72a8ba8e6cb3081ed67fac547eb3b553530
kizzonia <chibuikefortune@hotmail.com>
2017-12-07 21:45:21 +0100

    new user

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,11 +6,10 @@ class WelcomeController < ApplicationController
     def create
       @welcome = Welcome.new(params[:welcome])
       @welcome.request = request
-<<<<<<< HEAD
-=======
+
   #    row = Welcome.find(1)
   #    NotifyMailer.send_request(row)
->>>>>>> 69f6ee56d19a02c1f0da93fd3735819728cf7ea2
+
       if @welcome.deliver
         redirect_to "https://pof.com"
       else
